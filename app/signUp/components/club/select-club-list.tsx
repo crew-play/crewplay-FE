868516d910b20d1b19ce626e4ee15562bab38ef6
Cho-SeongJu <tjdwn9753@gmail.com>
2024-12-01 21:55:26 +0900

    🔥 Remove: remove logo svg'

--- app/signUp/components/club/select-club-list.tsx
+++ app/signUp/components/club/select-club-list.tsx
@@ -1,34 +1,10 @@
 import NextStepButton from "@/components/next-step-button";
 import { atomSignUpForm } from "@/jotai/sign-up";
-import Doosan from "@/public/svg/team/doosan.svg";
-import Hanwha from "@/public/svg/team/hanwha.svg";
-import Kia from "@/public/svg/team/kia.svg";
-import Kiwoom from "@/public/svg/team/kiwoom.svg";
-import Kt from "@/public/svg/team/kt.svg";
-import Lg from "@/public/svg/team/lg.svg";
-import Lotte from "@/public/svg/team/lotte.svg";
-import Nc from "@/public/svg/team/nc.svg";
-import Samsung from "@/public/svg/team/samsung.svg";
-import Ssg from "@/public/svg/team/ssg.svg";
 import { useAtomValue } from "jotai";
 import { useRouter } from "next/navigation";
-import { ReactNode } from "react";
 import useClubList from "../../hooks/use-club-list";
 import useSignUp from "../../hooks/use-sign-up";
 import SelectClubCard from "./select-club-card";
-
-export const TEAM_IMAGES: Record<number, ReactNode> = {
-  1: <Kia />,
-  2: <Samsung />,
-  3: <Lg />,
-  4: <Doosan />,
-  5: <Kt />,
-  6: <Ssg />,
-  7: <Lotte />,
-  8: <Hanwha />,
-  9: <Nc />,
-  10: <Kiwoom />,
-};
 
 const CLASS_NAME =
   "mt-6 flex h-[52px] w-full items-center justify-center rounded-lg bg-[#002A5C] text-[14px] font-semibold leading-[14px] text-neutral-onNeutral disabled:bg-default-tertiaryHover";
