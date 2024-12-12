b04ec1ab9294d25e74a1f3a42d5ead8a3ac61dcf
Cho-SeongJu <tjdwn9753@gmail.com>
2024-12-12 18:30:54 +0900

    💄 Design: update footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,16 +2,14 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="mx-auto flex h-[5.25rem] items-center justify-between border-t border-white-005 bg-white-004 px-[16px] text-[10px] leading-[17.38px] md:w-[620px] md:text-sm md:leading-[20.27px] lg:w-[920px] xl:w-[1220px] 2xl:w-[1520px]">
-      <div className="flex">
-        <Link href="/" className="font-normal text-gray-002">
-          서비스 이용약관
-        </Link>
-        <Link href="/" className="ml-4 font-bold">
+    <footer className="flex w-screen flex-col justify-between border-t border-t-gray-009 bg-white-001 px-[24px] py-[32px] text-[14px] leading-[19.6px] text-gray-002 lg:flex-row lg:items-center lg:px-[160px]">
+      <div className="mb-[8px] flex grow font-bold lg:mb-0">
+        <Link href="/">서비스 이용약관</Link>
+        <Link href="/" className="ml-[8px] lg:ml-[16px]">
           개인정보 처리방침
         </Link>
       </div>
-      <p className="font-normal text-gray-002">ⓒ 2024. CREWPLAY</p>
+      <p className="w-auto">ⓒ 2024. CREWPLAY</p>
     </footer>
   );
 }
