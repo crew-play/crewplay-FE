7dd0653d2b3ba17837f8052d238dc62968115c90
Cho-SeongJu <tjdwn9753@gmail.com>
2024-12-15 23:38:50 +0900

    ♻️ Refactor: refactor interceptor

--- api/interceptor.ts
+++ api/interceptor.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { reissueToken } from "./reissue-token";
-import { getToken } from "@/utils/token";
+import { getToken, removeToken } from "@/utils/token";
 
 let retryCount = 0;
 const MAX_RETRY_COUNT = 3;
@@ -37,15 +37,28 @@ instance.interceptors.response.use(
           return Promise.reject(refreshError);
         }
       }
-      // case 403: {
-      //   try {
-      //     const accessToken = await reissueToken();
-      //     error.config.headers["access"] = accessToken;
-      //     return instance(error.config);
-      //   } catch (refreshError) {
-      //     return Promise.reject(refreshError);
-      //   }
-      // }
+      case 403: {
+        if (retryCount < MAX_RETRY_COUNT) {
+          retryCount++;
+
+          const accessToken = await reissueToken();
+          localStorage.setItem("access", accessToken);
+
+          error.config.headers["access"] = accessToken;
+
+          return new Promise((resolve) => {
+            resolve(instance(error.config));
+          });
+        } else {
+          retryCount = 0;
+          localStorage.removeItem("access");
+          await removeToken("refresh");
+
+          alert("재 로그인이 필요합니다. 로그인 화면으로 이동합니다.");
+
+          window.location.href = "/login";
+        }
+      }
       case 500: {
         if (retryCount < MAX_RETRY_COUNT) {
           retryCount++;
