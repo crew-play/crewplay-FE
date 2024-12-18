f5692d710a1bb994c801076ee86a18aed8913f6c
Cho-SeongJu <tjdwn9753@gmail.com>
2024-12-19 00:29:22 +0900

    ✨ Feature: add link tag

--- components/header/header-profile.tsx
+++ components/header/header-profile.tsx
@@ -2,18 +2,21 @@ import { atomUserAuth } from "@/jotai/user-auth";
 import Profile from "@/public/svg/profile.svg";
 import { useAtomValue } from "jotai";
 import BottomArrow from "@/public/svg/bottom-arrow.svg";
+import Link from "next/link";
 
 export default function HeaderProfile() {
   const userAuth = useAtomValue(atomUserAuth);
 
   return (
-    <div className="flex cursor-pointer items-center">
-      <div className="flex size-[36px] items-center justify-center rounded-full bg-gray-009">
-        <Profile />
-      </div>
-      <span className="ml-[10px] text-[16px] font-medium leading-[22.4px]">
-        {userAuth.nickname}
-      </span>
+    <div>
+      <Link href="/my-page" className="flex items-center">
+        <div className="flex size-[36px] items-center justify-center rounded-full bg-gray-009">
+          <Profile />
+        </div>
+        <span className="ml-[10px] text-[16px] font-medium leading-[22.4px]">
+          {userAuth.nickname}
+        </span>
+      </Link>
     </div>
   );
 }
