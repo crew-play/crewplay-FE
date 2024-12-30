0521e5feecf3836cf8f62aefa254fbe2f3b5cca6
Cho-SeongJu <tjdwn9753@gmail.com>
2024-12-30 23:53:10 +0900

    ✨ Feature: add infinity query

--- app/today-news/components/today-news-list.tsx
+++ app/today-news/components/today-news-list.tsx
@@ -1,18 +1,20 @@
 import { INews } from "@/api/today-news";
 import TodayNewsCard from "./today-news-card";
 import MainButton from "@/components/main-button";
+import NotExist from "@/components/not-exist";
 
 interface ITodayNewsListProps {
   readonly todayIssueList: INews[];
+  readonly isExist: boolean;
+  readonly handleClickMoreButton: () => void;
 }
 
-export default function TodayNewsList({ todayIssueList }: ITodayNewsListProps) {
-  const handleClickMoreButton = () => {
-    // eslint-disable-next-line no-console
-    console.log("!!");
-  };
-
-  return (
+export default function TodayNewsList({
+  todayIssueList,
+  isExist,
+  handleClickMoreButton,
+}: ITodayNewsListProps) {
+  return isExist ? (
     <div>
       <div className="grid grid-cols-1 justify-items-center gap-y-[20px] lg:grid-cols-3 lg:justify-items-start lg:gap-[40px]">
         {todayIssueList.map((issue, index) => {
@@ -30,6 +32,10 @@ export default function TodayNewsList({ todayIssueList }: ITodayNewsListProps) {
       <div className="mb-[40px] mt-[20px] lg:mb-[106px] lg:mt-[60px]">
         <MainButton text="더보기" onClick={handleClickMoreButton} />
       </div>
+    </div>
+  ) : (
+    <div className="flex min-h-[calc(100vh-164px)] items-center">
+      <NotExist text="등록된 뉴스가 없습니다." />
     </div>
   );
 }
