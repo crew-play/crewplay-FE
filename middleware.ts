70c022971f15c465f066d61841f7c17acc9516f9
Cho-SeongJu <tjdwn9753@gmail.com>
2025-01-06 19:48:26 +0900

    📦 Chore: middleware and CORS config

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = (request: NextRequest) => {
-  const response = NextResponse.next();
+  return NextResponse.redirect(new URL("/", request.url));
+};
 
-  if (
-    request.nextUrl.pathname.startsWith("/my-page") ||
-    request.nextUrl.pathname.startsWith("/signUp") ||
-    request.nextUrl.pathname.startsWith("/signUp/success")
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  return response;
+export const config = {
+  matcher: ["/my-page", "/signUp/:path*"],
 };
--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,28 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["image.imnews.imbc.com"],
   },
+
+  async headers() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
